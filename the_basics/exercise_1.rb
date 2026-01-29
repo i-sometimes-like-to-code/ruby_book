47ecd7be71e1cf16f552bc4951fbecbf8e906a51
i-sometimes-like-to-code <246318456+i-sometimes-like-to-code@users.noreply.github.com>
2026-01-28 20:25:47 -0500

    adding the_basics exercises

---
+++ the_basics/exercise_1.rb
@@ -0,0 +1,10 @@
+first_name = "Hello"
+last_name = "Sunshine"
+
+full_name = "#{first_name} #{last_name}"
+full_name2 = first_name + ' ' + last_name
+full_name3 = "Hello " + "Sunshine"
+
+puts full_name
+puts full_name2
+puts full_name3
