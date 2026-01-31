0df1c91fa986e769c087edc664aa45526360aa41
i-sometimes-like-to-code <246318456+i-sometimes-like-to-code@users.noreply.github.com>
2026-01-30 19:59:28 -0500

    exercise4 for the_basics

---
+++ the_basics/exercise_4.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal = true
+
+arr_of_dates = [1975, 2004, 2013, 2001, 1981]
+
+arr_of_dates.each { |year| puts year }
+
+##########################
+# LS Solution
+# dates = [1975, 2004, 2013, 2001, 1981]
+#
+# puts dates[0]
+# puts dates[1]
+# puts dates[2]
+# puts dates[3]
+# puts dates[4]
