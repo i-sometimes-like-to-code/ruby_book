e9f74306812066ce7c215469e21534f7cc182465
i-sometimes-like-to-code <246318456+i-sometimes-like-to-code@users.noreply.github.com>
2026-01-30 20:41:15 -0500

    age.rb exercise_2 for variables

---
+++ variables/age.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal = true
+
+puts "How old are you?"
+age = gets.chomp
+puts "In 10 years you will be #{age.to_i + 10}"
+puts "In 20 years you will be #{age.to_i + 20}"
+puts "In 30 years you will be #{age.to_i + 30}"
+puts "In 40 years you will be #{age.to_i + 40}"
+
+
+####################################3
+# # LS
+# puts "How old are you?"
+# age = gets.chomp.to_i
+# puts "In 10 years you will be:"
+# puts age + 10
+# puts "In 20 years you will be:"
+# puts age + 20
+# puts "In 30 years you will be:"
+# puts age + 30
+# puts "In 40 years you will be:"
+# puts age + 40
