374774a4a18610970561117a458ece20ff61071c
i-sometimes-like-to-code <246318456+i-sometimes-like-to-code@users.noreply.github.com>
2026-01-30 19:53:51 -0500

    exercise_3 for the_basics

---
+++ the_basics/exercise_3.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+movie_list = { 'The Godfather': 1972, 'Parasite': 2019, 'Finding Nemo': 2003 }
+
+puts movie_list.values
+
+puts movie_list[:'The Godfather']
+
+
+########################################
+# Launch School solution
+#
+# movies = { :jaws => 1975,
+#            :anchorman => 2004,
+#            :man_of_steel => 2013,
+#            :a_beautiful_mind => 2001,
+#            :the_evil_dead => 1981 }
+#
+# puts movies[:jaws]
+# puts movies[:anchorman]
+# puts movies[:man_of_steel]
+# puts movies[:a_beautiful_mind]
+# puts movies[:the_evil_dead]
