814a7254bc9703a57227fa88d955bded667205bb
i-sometimes-like-to-code <246318456+i-sometimes-like-to-code@users.noreply.github.com>
2026-01-30 20:37:42 -0500

    exercise_1 variables name.rb

---
+++ variables/exercise_1.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal = true
+
+puts "Please type in your name"
+name = gets.chomp
+
+puts "Greetings #{name}. Welcome!"
+
+###################################
+# LS
+# name.rb
+#
+# puts "What is your name?"
+# name = gets.chomp
+# puts "Hello " + name
